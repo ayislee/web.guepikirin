1b68a58cfc204696fea5574f708ac495df075ca8
Ayi Restudiputra <ayislee001@gmail.com>
2023-07-06 12:00:09 +0700

    soming soon

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,18 +2,48 @@ import Head from 'next/head'
 import Layout from '../components/Layout'
 import {Container, Button} from 'react-bootstrap'
 import { useRouter } from 'next/router'
+import comming from '../components/images/coming-soon.jpg'
+import commingMobile from '../components/images/coming-soon-mobile.jpg'
+import React, { useState, useEffect } from 'react';
 
 export default function Home() {
     const router = useRouter()
+    const [imageBackground,setImageBackground] = useState()
+    useEffect(()=>{
+        console.log("width",window.innerWidth)
+        if(window.innerWidth > 640) {
+            console.log('desktop')
+            setImageBackground(comming.src)
+        }else {
+            console.log('mobile')
+            setImageBackground(commingMobile.src)
+        }
+
+        window.addEventListener('resize',()=>{
+            console.log("width",window.innerWidth)
+            if(window.innerWidth > 640) {
+                setImageBackground(comming.src)
+            }else {
+                setImageBackground(commingMobile.src)
+            }
+            
+        })
+    },[])
     return (
-        <Layout>
+        // <Layout>
+        //     <Container>
+        //         <div className="text-center margin-top">
+        //             <div className="title">Ready for some real growth?</div>
+        //             <div className="sub-title">Let’s figure out what you need, shall we?</div>
+        //             <Button className="own-btn mt-3" onClick={()=>router.push('/survey')}>Let’s Talk</Button>
+        //         </div>
+        //     </Container>
+        // </Layout>
+
+        <div>
             <Container>
-                <div className="text-center margin-top">
-                    <div className="title">Ready for some real growth?</div>
-                    <div className="sub-title">Let’s figure out what you need, shall we?</div>
-                    <Button className="own-btn mt-3" onClick={()=>router.push('/survey')}>Let’s Talk</Button>
-                </div>
+                <img src={imageBackground} alt="right" width="100%"/>
             </Container>
-        </Layout>
+        </div>
     )
 }
